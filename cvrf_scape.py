f8969beea3eec0d809922c1f0e9d0193aad4cb93
Seb Rupik <sebrupik@gmail.com>
2019-10-20 22:52:11 +0100

    building the DB schemas

--- cvrf_scape.py
+++ cvrf_scape.py
@@ -5,7 +5,20 @@ import wget
 import xmltodict
 import re
 import requests
+import sqlite3
 
+CREATE_CVRF_DETAIL = "CREATE TABLE cvrf_detail (pk INTEGER UNIQUE, DocumentTitle TEXT, DocumentType TEXT," + \
+                     "InitialReleaseDate TEXT, CurrentReleaseDate TEXT, Summary INTEGER, AffectedProducts TEXT," + \
+                     "VulnerableProducts TEXT, ConfirmedNotVulnerableProducts TEXT, Details TEXT, Workarounds TEXT," + \
+                     "FixedSoftware TEXT, VulnerabilityPolicy TEXT, ExploitationAnnouncement TEXT, Source TEXT," + \
+                     "ReferenceURL TEXT, PRIMARY KEY(pk AUTOINCREMENT))"
+CREATE_DOCUMENT_TRACKING = "CREATE TABLE document_tracking (pk INTEGER UNIQUE, cvrf_pk INTEGER, revision INTEGER," + \
+                           "date TEXT, description TEXT, PRIMARY KEY(pk AUTOINCREMENT))"
+CREATE_VENDOR_IDS = "CREATE TABLE vendor_ids (pk INTEGER UNIQUE, Name TEXT, PRIMARY KEY(pk AUTOINCREMENT))"
+CREATE_PRODUCT_NAMES = "CREATE TABLE product_names (pk INTEGER UNIQUE, ProductName TEXT," + \
+                       "PRIMARY KEY(pk AUTOINCREMENT))"
+CREATE_PRODUCT_IDS = "CREATE TABLE product_ids (pk INTEGER UNIQUE, CVRFPID TEXT, Name TEXT, FullName INTEGER," + \
+                     "PRIMARY KEY(pk AUTOINCREMENT))"
 
 HTML_REGEX = "^\s*<(a href=\")(?P<href>.*)\"\starget.*class=\"ng-binding\">(?P<name>.*)</a>$"
 
@@ -28,7 +41,25 @@ def gather_cvrfs():
                     print(json.dumps(xmltodict.parse(f2.read()), indent=4))
 
 
+#def update_db_with_cvrf():
+
+
 def main():
+    con = sqlite3.connect('cvrfs.db')
+    _cursor = con.cursor()
+    _cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='cvrf_detail'")
+    row = _cursor.fetchone()
+
+    if row is None:
+        # build the tables
+        con = sqlite3.connect('cvrfs.db')
+        _cursor = con.cursor()
+        _cursor.execute(CREATE_CVRF_DETAIL)
+        _cursor.execute(CREATE_DOCUMENT_TRACKING)
+        _cursor.execute(CREATE_VENDOR_IDS)
+        _cursor.execute(CREATE_PRODUCT_NAMES)
+        _cursor.execute(CREATE_PRODUCT_IDS)
+
     gather_cvrfs()
 
 
